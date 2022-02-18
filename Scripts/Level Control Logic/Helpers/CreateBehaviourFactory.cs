b0b2f63928ddf6055da889fd54b03fd64a9cece4
at-grigorievich <at.grigorievich@gmail.com>
2022-02-18 16:30:23 +0300

    Encapsulated level creation logic

---
+++ Scripts/Level Control Logic/Helpers/CreateBehaviourFactory.cs
@@ -0,0 +1,35 @@
+﻿namespace ATG.LevelControl
+{
+    public static class CreateBehaviourFactory
+    {
+        public static ICreateLevelBehaviour Execute
+            (LevelType type,EnvironmentBlock.Factory factory)
+        {
+            ICreateLevelBehaviour levelBehaviour = null;
+            switch (type)
+            {
+                case LevelType.Line : 
+                    levelBehaviour = new LineLevelBehaviour();
+                    break;
+                case LevelType.Matrix :
+                    levelBehaviour = new MatrixLevelBehaviour();
+                    break;
+                case LevelType.Static :
+                    levelBehaviour = new StaticLevelBehaviour();
+                    break;
+                
+                case LevelType.ZenjectLine :
+                    levelBehaviour = new ZenjectLineLevelBehaviour<EnvironmentBlock>(factory);
+                    break;
+                case LevelType.ZenjectMatrix :
+                    levelBehaviour = new ZenjectMatrixLevelBehaviour<EnvironmentBlock>(factory);
+                    break;
+                case LevelType.ZenjectStatic :
+                    levelBehaviour = new ZenjectStaticLevelBehaviour<EnvironmentBlock>(factory);
+                    break;
+            }
+
+            return levelBehaviour;
+        }
+    }
+}
